25eec08b50b7e4eb5484526dcb18462e30b86fc4
Mark <mark@trendpc.com.au>
2011-07-21 23:13:32 +1000

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTest::Application.config.secret_token = '9816b933bf70c301f117e540d968e04bb2b2e6440d1fd6ede5b81de90a12bc6ae22a9a937de8b2a91eb2665cb7f626d671abf40c2cd02daffda88d9d097392c9'
